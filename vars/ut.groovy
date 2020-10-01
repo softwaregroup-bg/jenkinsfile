8297732cdc5ca4557da789a996e026ea736d7538
Kalin Krustev <kalin.krustev@gmail.com>
2020-10-01 10:53:35 +0300

    feat: improve email reports

--- vars/ut.groovy
+++ vars/ut.groovy
@@ -60,12 +60,14 @@ def call(Map params = [:]) {
                         ])
                         emailext(
                             mimeType: 'text/html',
-                            body: '''<h1>Jenkins build ${JOB_NAME} ${BUILD_DISPLAY_NAME}</h1>
+                            body: '''<h1>Jenkins build ''' + repoUrl.replaceAll(/^[^\/]*\/|.git$/, "") + ''' ${BUILD_DISPLAY_NAME}</h1>
 <h2><b>Status</b>: ${BUILD_STATUS}</h2>
 <b>Trigger</b>:  ${CAUSE}<br>
 <b>Job</b>: ${JOB_URL}<br>
+<b>Branch</b>: ''' + repoUrl.replaceAll(/^git@|.git$/, '').replace(':', '/') + '''/tree/${GIT_BRANCH}<br>
+<b>MR/PR</b>: ${CHANGE_URL}<br>
 <b>Summary</b>: ${BUILD_URL}<br>
-<b>Console</b>: ${BUILD_URL}/console<br>
+<b>Console</b>: ${BUILD_URL}console<br>
 <b>Workspace</b>: ${BUILD_URL}/execution/node/4/ws<br>
 <b>Sonar</b>: ''' + scanner.dashboardUrl + '''<br>
 <b>Checkstyle</b>: ${CHECKSTYLE_RESULT}<br>
@@ -78,7 +80,7 @@ ${FILE,path=".lint/test.txt"}
 <b>npm audit</b>:${FILE,path=".lint/audit.html"}
             ''',
                             recipientProviders: [[$class: 'CulpritsRecipientProvider'],[$class: 'RequesterRecipientProvider']],
-                            subject: 'Build ${BUILD_STATUS} in Jenkins: ${JOB_NAME} ${BUILD_DISPLAY_NAME} (' + currentBuild.durationString +')'
+                            subject: 'Build ${BUILD_STATUS} in Jenkins: ' + repoUrl.replaceAll(/^[^\/]*\/|.git$/, "") + ' ${BUILD_DISPLAY_NAME} (' + currentBuild.durationString +')'
                         )
                     }
                     failure {
