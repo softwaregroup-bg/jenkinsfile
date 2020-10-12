078ae32be3e4a87024826aecdeed02510bcd3839
Kalin Krustev <kalin.krustev@softwaregroup.com>
2020-10-12 15:55:56 +0300

    pass CHANGE_ID

--- resources/ut.sh
+++ resources/ut.sh
@@ -100,6 +100,7 @@ docker run -u node:node -i --rm \
     -e BUILD_CAUSE=$BUILD_CAUSE \
     -e DOCKER_USR=$DOCKER_USR \
     -e DOCKER_PSW=$DOCKER_PSW \
+    -e CHANGE_ID=$CHANGE_ID \
     -e ${UT_PREFIX}_db__create__password=$UT_DB_PASS \
     -e ${UT_PREFIX}_db__connection__encryptionPass="$encryptionPass" \
     -e ${UT_PREFIX}_db__connection__database=${UT_MODULE}-${UT_PROJECT}-${BRANCH_NAME//[\/\\]/-}-${BUILD_NUMBER}${DBSUFFIX} \
