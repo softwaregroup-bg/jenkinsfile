8cf3019447262361540d86fda47c5e2d3de9bc47
Georgi <gkostadi@gmail.com>
2020-12-21 11:57:37 +0200

    [BR-730] Fix nexus port (#13)

--- resources/ut.sh
+++ resources/ut.sh
@@ -131,7 +131,7 @@ docker run -u node:node -i --rm \
 docker run -u node:node -i --rm \
     --cap-add=SYS_ADMIN \
     -v "$(pwd)/.lint:/app/.lint" \
-    nexus-dev.softwaregroup.com:5001/softwaregroup/capture-website --output=.lint/sonar.png --width=1067 --height=858 --scale-factor=0.6 \
+    nexus-dev.softwaregroup.com:5000/softwaregroup/capture-website --output=.lint/sonar.png --width=1067 --height=858 --scale-factor=0.6 \
     https://sonar.softwaregroup.com/dashboard?id=${UT_PROJECT}%3A${GIT_BRANCH//[\/\\]/%2F}
 if [[ $RELEASE && ${UT_IMPL} ]]; then
     TAG=${RELEASE//[\/\\]/-}
