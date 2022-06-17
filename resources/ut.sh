3fe817e4fbe3c2a9542ac3f40d3e92acbdc0af45
Kalin Krustev <kalin.krustev@gmail.com>
2022-06-17 08:31:10 +0300

    chore: #UTCORE-121

--- resources/ut.sh
+++ resources/ut.sh
@@ -90,7 +90,7 @@ ${PREFETCH}
 COPY --chown=node:node package.json package.json
 RUN --mount=type=cache,target=/tmp/app/.npm,mode=0777,uid=1000,gid=1000 \
   mkdir -p /app/node_modules/.cache \
-  && npm update -g ut-tools ut-storybook ut-webpack ut-help \
+  && npm update -g --legacy-peer-deps ut-tools ut-storybook ut-webpack ut-help \
   && npm --legacy-peer-deps install
 COPY --chown=node:node . .
 EOF
