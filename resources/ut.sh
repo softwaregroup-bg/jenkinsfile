24701380348bb61e6a23a3fdd79f32498bae9266
dilyanVasilev <dilyan.vasilev@gmail.com>
2025-08-20 11:56:20 +0300

    Update ut.sh

--- resources/ut.sh
+++ resources/ut.sh
@@ -147,7 +147,7 @@ docker run -u node:node -i \
     ${UT_PROJECT}:${TEST_IMAGE_TAG} -c "(git checkout -- .dockerignore || true) && npm run jenkins" \
     || (docker rm ${UT_PROJECT}-${TEST_IMAGE_TAG} && false)
 docker cp ${UT_PROJECT}-${TEST_IMAGE_TAG}:/app/package.json package.json
-docker cp ${UT_PROJECT}-${TEST_IMAGE_TAG}:/app/. app
+docker cp ${UT_PROJECT}-${TEST_IMAGE_TAG}:/app app
 docker rm ${UT_PROJECT}-${TEST_IMAGE_TAG}
 
 SONAR_BRANCH=-Dsonar.branch.name=${GIT_BRANCH#origin/}
@@ -193,7 +193,7 @@ EOF
         COPY --chown=node:node --from=${UT_PROJECT}:${IMAGE_TAG} /app /app
         COPY --chown=node:node --from=${UT_PROJECT}:${IMAGE_TAG} /home/node/.cache/ms-playwright /home/node/.cache/ms-playwright
         WORKDIR /app
-        COPY --chown=node:node app/* .
+        COPY --chown=node:node app/. .
         COPY --chown=node:node dist dist
         COPY --chown=node:node package.json package.json
         ENTRYPOINT ["node", "index.js"]
