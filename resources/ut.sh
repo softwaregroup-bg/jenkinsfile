ed1b6eda9ab812f17b4c7773eb969d66ebf3e396
dilyanVasilev <dilyan.vasilev@gmail.com>
2025-08-20 11:53:44 +0300

    Update ut.sh

--- resources/ut.sh
+++ resources/ut.sh
@@ -147,7 +147,7 @@ docker run -u node:node -i \
     ${UT_PROJECT}:${TEST_IMAGE_TAG} -c "(git checkout -- .dockerignore || true) && npm run jenkins" \
     || (docker rm ${UT_PROJECT}-${TEST_IMAGE_TAG} && false)
 docker cp ${UT_PROJECT}-${TEST_IMAGE_TAG}:/app/package.json package.json
-docker cp ${UT_PROJECT}-${TEST_IMAGE_TAG}:/app/* app
+docker cp ${UT_PROJECT}-${TEST_IMAGE_TAG}:/app/. app
 docker rm ${UT_PROJECT}-${TEST_IMAGE_TAG}
 
 SONAR_BRANCH=-Dsonar.branch.name=${GIT_BRANCH#origin/}
